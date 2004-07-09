7cad90f9a0b78e4b45f95aaba042a0c22fd94fff
Matthew Brett <matthew.brett@gmail.com>
2004-07-09 23:51:42 +0000

    Version to deal with negative filter delays

--- @phiw_wavelet/private/do_iwtx.c
+++ @phiw_wavelet/private/do_iwtx.c
@@ -4,11 +4,7 @@ do first scale inverse wavelet transform in x dimension of matrix
 FORMAT t = do_iwtx(t, h, g, dlp, dhp, reco_detail)
 See the do_iwtx.m file for detail
 
-Todo
-----
-accept negative filter delays
-
-$Id: do_iwtx.c,v 1.2 2004/07/09 16:34:38 matthewbrett Exp $ 
+$Id: do_iwtx.c,v 1.3 2004/07/09 23:51:42 matthewbrett Exp $ 
 
 */ 
 
@@ -30,27 +26,32 @@ $Id: do_iwtx.c,v 1.2 2004/07/09 16:34:38 matthewbrett Exp $
 #define GI      1
 #define N       2
 
-/* max min odd minifunctions */
-int my_max(int a, int b) {
-  return a > b ? a : b;
+/* max min ceil floor minifunctions */
+int max0(int a, int b) { /* returns max of a,b,0 */
+  a = a > b ? a : b;
+  return a > 0 ? a : 0;
 }
-int my_min(int a, int b) {
-  return a > b ? b : a;
+int ceil2(int a) {
+  int c;
+  c = a & 1;
+  return (a > 0 ? a/2 + c : a/2);
 }
-int my_isodd(int a) {
-  return (a & 1);
+int floor2(int a) {
+  int c;
+  c = a & 1;
+  return (a > 0 ? a/2: a/2 - c);
 }
 
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
-  double	*m, *h, *g, *iwm, *f_ptr, *buf, *iwm_out_ptr, *filters[N], *delays[N];
-  double        *m_ptr, *iwm_ptr, *col_ptr, *in_ptr, *out_ptr, *f, *pos, *f_pos;
-  double        *d_start, *d_end, *d_start_h, *d_start_g, *d_end_h, *d_end_g;
-  double        d, res1, res2;
-  int           dlp, dhp, dlp_12, dhp_12, odd_dlp, odd_dhp, st_wrap, n_cols, col;
-  int           st_wrap_h, st_wrap_g, len_mid, odd_delay, len_x_12_m, delay_p;
-  int           reco_detail, len_h_12, len_g_12, delay, fno, odd_f, len_f, len_f_p; 
-  int		len_x, len_x_12, f_lengths[N], len_buf, i, j, odd_h, odd_g;
+  double	*m, *iwm, *f_ptr, *buf, *iwm_out_ptr;
+  double        *m_ptr, *iwm_ptr, *col_ptr, *in_ptr, *out_ptr, *f[N], *pos, *f_pos;
+  double        *dat_start[N], *dat_end[N], *st_ptr, *end_ptr, *lp_f;
+  double        val, res1, res2;
+  int           d[N], f_len_f_12[N], c_len_f_12[N], odd_f[N], odd_d[N], st_d[N], end_d[N];
+  int           st_wrap[N], len_mid, odd_delay, len_x_12_m, n_cols, col;
+  int           reco_detail, fno, lp_f_len_f_12, lp_c_len_f_12, lp_odd_f, lp_st_d; 
+  int		lp_end_d, lp_st_wrap, len_x, len_x_12, len_f[N], len_buf, i, j;
   long int      size_m;
   
   if (nrhs < 5) 
@@ -66,33 +67,38 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
   else reco_detail = 1;
 
   /* Assign pointers to the parameters */
-  m                = mxGetPr(M);
-  filters[HI]      = mxGetPr(RH);
-  filters[GI]      = mxGetPr(RG);
+  m          = mxGetPr(M);
+  f[HI]      = mxGetPr(RH);
+  f[GI]      = mxGetPr(RG);
 
   /* get delays */
-  delays[HI]       = (int)*(mxGetPr(DLP));
-  delays[GI]       = (int)*(mxGetPr(DHP));
+  d[HI]       = (int)*(mxGetPr(DLP));
+  d[GI]       = (int)*(mxGetPr(DHP));
 
   /* get dimensions and lengths*/
   len_x    = mxGetM(M);
   n_cols   = mxGetN(M);
   size_m   = len_x * n_cols;
-  f_lengths[HI] = mxGetM(RH) * mxGetN(RH);
-  f_lengths[GI] = mxGetM(RG) * mxGetN(RG);
+  len_f[HI] = mxGetM(RH) * mxGetN(RH);
+  len_f[GI] = mxGetM(RG) * mxGetN(RG);
 
   /* check to-be-transformed length divisible by 2 */
-  if (my_isodd(len_x))
+  if (len_x & 1)
     mexErrMsgTxt("Length of x dimension must be divisible by 2.");    
   len_x_12 = len_x/2;
 
   /* do delay etc calculations to save time later in main loop */
-  for (fno=0; fno<N, fno++) {
-    delays_12[fno]   = delays[fno]/2;
-    odd_dlp  = my_isodd(dlp);
-    len_h_12 = len_h/2;
-    odd_h    = my_isodd(len_h);
-    st_wrap[fno] = my_max((len_h_12 + odd_h - dlp_12 - 1), 0); 
+  for (fno=0; fno<N; fno++) {
+    /* filter length divided by 2 */
+    c_len_f_12[fno] = ceil2(len_f[fno]);
+    f_len_f_12[fno] = floor2(len_f[fno]);
+    odd_f[fno]      = len_f[fno] & 1;
+
+    /* delays */
+    odd_d[fno]      = d[fno] & 1;
+    st_d[fno]       = floor2(d[fno]);    /* delay offset at start */
+    end_d[fno]      = ceil2(d[fno]);     /* delay offset at end   */ 
+    st_wrap[fno]    = max0((c_len_f_12[fno] - st_d[fno] - 1), 0);
   }
 
   /* make output matrix */
@@ -105,29 +111,27 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
   for (i=0; i<size_m; i++)
     *(iwm_ptr++) = *(m_ptr++);
 
-  /* make buffer to contain whole row sample, wraparounds */
-  st_wrap_h = my_max((len_h_12 + odd_h - dlp_12 - 1), 0); /* start wrap size for h */
-  st_wrap_g = my_max((len_g_12 + odd_g - dhp_12 - 1), 0); /* start wrap size for g */
-  /* gap between the high and low pass bits must be as long as low-pass delay */
-  /* but we can use the low pass bit to contain the start wrap for high pass  */
-  len_mid   = my_max(dlp_12 + odd_dlp, st_wrap_g - (st_wrap_h + len_x_12));
-  len_buf   = st_wrap_h + len_x_12 + len_mid + len_x_12 + dhp_12 + odd_dhp;
+  /* make buffer to contain whole row sample, wraparounds gap between
+  the high and low pass bits must be as long as low-pass delay but we
+  can use the low pass memory to contain the start wrap for high pass  */
+  len_mid     = max0(end_d[HI], st_wrap[GI] - (st_wrap[HI] + len_x_12));
+  len_buf     = st_wrap[HI]  + len_x_12 + len_mid + len_x_12 + max0(end_d[GI], 0);
   buf = (double *)mxCalloc(len_buf, sizeof(double));
-  d_start_h = buf + st_wrap_h;
-  d_end_h   = d_start_h + len_x_12 - 1;
-  d_start_g = d_end_h + 1 + len_mid;
-  d_end_g   = d_start_g + len_x_12 - 1;
+  dat_start[HI] = buf + st_wrap[HI];
+  dat_end[HI]   = dat_start[HI] + len_x_12 - 1;
+  dat_start[GI] = dat_end[HI] + 1 + len_mid;
+  dat_end[GI]   = dat_start[GI] + len_x_12 - 1;
   
   /* for each x column */
   col_ptr = iwm;
   for (col=0; col < n_cols; col++) {
     /* copy the row data to buffer */
-    out_ptr = d_start_h;
+    out_ptr = dat_start[HI];
     in_ptr  = col_ptr;
     for (i=0; i<len_x_12; i++)
       *(out_ptr++) = *(in_ptr++);
     if (reco_detail) {
-      out_ptr = d_start_g;
+      out_ptr = dat_start[GI];
       for (i=0; i<len_x_12; i++)
 	*(out_ptr++) = *(in_ptr++);
     }
@@ -135,58 +139,48 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
     /* for each of the low pass and high pass filters, if required */
     for (fno=0; fno<= reco_detail; fno++) {
       iwm_out_ptr = col_ptr;
-      
-      if (fno) { /* detail scale */
-	len_f  = len_g_12;
-	odd_f  = odd_g;
-	delay  = dhp_12;
-	odd_delay = odd_dhp;
-	f       = g;
-	st_wrap = st_wrap_g;
-	d_start = d_start_g;
-	d_end   = d_end_g;
-      } else {   /* low pass scale */
-	len_f  = len_h_12;
-	odd_f  = odd_h;
-	delay  = dlp_12;
-	odd_delay = odd_dlp;
-	f      = h;
-	st_wrap = st_wrap_h;
-	d_start = d_start_h;
-	d_end   = d_end_h;
-      }
+
+      /* loop variables for speed - probably overkill */
+      lp_f_len_f_12 = f_len_f_12[fno];
+      lp_c_len_f_12 = c_len_f_12[fno];
+      lp_odd_f  = odd_f[fno];
+      lp_st_d   = st_d[fno];
+      lp_end_d  = end_d[fno];
+      lp_st_wrap = st_wrap[fno];
+      odd_delay = odd_d[fno];
+      lp_f      = f[fno];
       len_x_12_m = len_x_12 - odd_delay;
-      len_f_p = len_f + odd_f;
-      delay_p = delay + odd_delay;
 
       /* make end wrap */
-      out_ptr = d_end + 1;
-      in_ptr  = d_start;
-      for (i=0; i < delay_p; i++) {
+      st_ptr  = dat_start[fno];
+      end_ptr = dat_end[fno];
+      out_ptr = end_ptr + 1;
+      in_ptr  = st_ptr;
+      for (i=0; i < lp_end_d; i++) {
 	*(out_ptr++) = *(in_ptr++);
-	if (in_ptr > d_end)
-	  in_ptr = d_start;
+	if (in_ptr > end_ptr)
+	  in_ptr = st_ptr;
       }
       /* make start wrap */
-      out_ptr = d_start-1;
-      in_ptr = d_end;
-      for (i=0; i < st_wrap; i++) {
+      out_ptr = st_ptr-1;
+      in_ptr  = end_ptr;
+      for (i=0; i < lp_st_wrap; i++) {
 	*(out_ptr--) = *(in_ptr--);
-	if (in_ptr < d_start)
-	  in_ptr = d_end;
+	if (in_ptr < st_ptr)
+	  in_ptr = end_ptr;
       }
 
       /* do convolution */
-      pos = d_start + delay;
+      pos = st_ptr + st_d[fno];
 
       /* do start conv for odd delay */
       if (odd_delay) {
       
 	/* move back along even part of filter */
-	f_ptr = f;
+	f_ptr = lp_f;
 	f_pos = pos++;
 	res2 = 0;
-	for (j=0; j<len_f; j++) {
+	for (j=0; j<lp_f_len_f_12; j++) {
 	  res2 += *(f_pos--) * *(++f_ptr);
 	  f_ptr++;
 	}
@@ -196,15 +190,15 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 
       /* move along row */
       for (i=0; i<len_x_12_m; i++) { /* taking into account odd_delay */
-	f_ptr = f;
+	f_ptr = lp_f;
 	f_pos = pos++;
 	res1 = 0;
 	res2 = 0;
 	/* move back along filter */
-	for (j=0; j<len_f; j++) {
-	  d = *f_pos--;
-	  res1 += d * *(f_ptr++);  /* odd portion of filter */
-	  res2 += d * *(f_ptr++);  /* even portion */
+	for (j=0; j<lp_f_len_f_12; j++) {
+	  val = *f_pos--;
+	  res1 += val * *(f_ptr++);  /* odd portion of filter */
+	  res2 += val * *(f_ptr++);  /* even portion */
 	}
 	if (odd_f)
 	  res1 += *(f_pos) * *(f_ptr);
@@ -220,10 +214,10 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
       /* do end conv for odd delay */
       if (odd_delay) {
 	/* move back along odd part of filter */
-	f_ptr = f;
+	f_ptr = lp_f;
 	f_pos = pos;
 	res1 = 0;
-	for (j=0; j<len_f_p; j++) {
+	for (j=0; j<lp_c_len_f_12; j++) {
 	  res1 += *(f_pos--) * *(f_ptr++);
 	  f_ptr++;
 	}
@@ -231,7 +225,6 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 	else     *(iwm_out_ptr++) = res1;
       } /* end conv */
       
-
     } /* for each filter */
 
     /* move to next row */
