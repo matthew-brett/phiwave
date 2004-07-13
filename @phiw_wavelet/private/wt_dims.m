581ae182a2f18038329b5dc4eb2e759e62a0d1d5
Matthew Brett <matthew.brett@gmail.com>
2004-07-13 01:52:00 +0000

    Cleanup, returns maximum scale available by default, checks scales

--- @phiw_wavelet/private/wt_dims.m
+++ @phiw_wavelet/private/wt_dims.m
@@ -5,7 +5,7 @@ function [wt_sz, scales_in_sz, scales_out_sz] = wt_dims(sz, scales)
 % Inputs 
 % sz                  - current matrix size (1 by ndims) (dimensions size 1 are
 %                       ignored) 
-% scales              - number of scales for wt
+% scales              - number of scales for wt (defaults to max)
 % 
 % Outputs 
 % wt_sz               - expected size (1 by ndims) after wt 
@@ -13,16 +13,16 @@ function [wt_sz, scales_in_sz, scales_out_sz] = wt_dims(sz, scales)
 % scales_in_sz        - size of each block of data (one pre scale) to be entered
 %                       into wt (scales by ndims)
 % scales_out_sz       - size of each block (one per scale) as output from wt
-% (scales by ndims) 
+%                       (scales by ndims) 
 %
-% The problem is that the wt fpr one scale and one
-% dimension may generate a matrix that is larger by 1 in the transformed
-% dimension, because the wt will generate a padding 0 at the end before
-% doing the wt, so that the ouputs can be decimated.  So we need to be able
-% to predict the various sizes as we go through the wt, in order to get and
-% set the data to/from the pre-wt and post-wt matrices.
+% The problem is that the wt fpr one scale and one dimension may generate a
+% matrix that is larger by 1 in the transformed dimension, because the wt
+% will generate a padding 0 at the end before doing the wt, so that the
+% ouputs can be decimated.  So we need to be able to predict the various
+% sizes as we go through the wt, in order to get and set the data to/from
+% the pre-wt and post-wt matrices.
 % 
-% $Id: wt_dims.m,v 1.1 2004/07/12 22:16:11 matthewbrett Exp $
+% $Id: wt_dims.m,v 1.2 2004/07/13 01:52:00 matthewbrett Exp $
   
 if nargin < 1
   error('Need input size(s)');
@@ -30,8 +30,9 @@ end
 in_dims = find(sz > 1);
 max_scale = min(ceil(log2(sz(in_dims))));
 if nargin < 2
-  scales = max_scale;
+  scales = [];
 end
+if isempty(scales), scales = max_scale; end
 if scales > max_scale
   error(sprintf(['Scale %d too high. Maximum scale ' ...
 		 'for these dimensions is %d'], scales, max_scale));
