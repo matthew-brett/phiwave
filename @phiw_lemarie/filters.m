83330ab5539666994a9e7de62db302ae436f4b8d
Matthew Brett <matthew.brett@gmail.com>
2004-07-14 20:33:09 +0000

    Forced lemarie filters to return filter of length divisible by 2

--- @phiw_lemarie/filters.m
+++ @phiw_lemarie/filters.m
@@ -1,10 +1,11 @@
 function [H,G,RH,RG] = filters(w, varargin)
 % returns filter for lemarie wavelet  
 %
-% $Id: filters.m,v 1.1 2004/06/25 15:20:43 matthewbrett Exp $
+% $Id: filters.m,v 1.2 2004/07/14 20:33:09 matthewbrett Exp $
 
 if nargin < 2
   error('Need image width to specify filter')
 end
-  
-[H,G,RH,RG] = lemarie(varargin{1}/w.phiw_wavelet.params);
+
+f_len = ceil(varargin{1}/w.phiw_wavelet.params/2)*2;
+[H,G,RH,RG] = lemarie(f_len);
