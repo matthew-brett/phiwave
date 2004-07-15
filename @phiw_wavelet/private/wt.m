1c3c115900fcc4fbe3c8833190d8527c48a4634e
Matthew Brett <matthew.brett@gmail.com>
2004-07-15 05:19:00 +0000

    Changed UviWave functions to be wrappers for wtnd and iwtnd

--- @phiw_wavelet/private/wt.m
+++ @phiw_wavelet/private/wt.m
@@ -1,41 +1,13 @@
 function y=wt(x,h,g,scales,del1,del2)
-
 % WT   Discrete Wavelet Transform.
 % 
-%      WT(X,H,G,SCALES) calculates the wavelet transform of vector X. 
-%      If X is a matrix (2D), WT will calculate the one dimensional 
-%      wavelet transform of each row vector. The second argument H 
-%      is the lowpass filter and the third argument G the highpass
-%      filter.
+% See wtnd for help on inputs and output format 
+% 
+% This function is a wrapper for wtnd, for compatibility with UviWave
 %
-%      The output vector contains the coefficients of the DWT ordered 
-%      from the low pass residue at scale K to the coefficients
-%      at the lowest scale, as the following example ilustrates:
+% See also:  WTND, IWT, IWTND, WTCENTER, ISPLIT.
 %
-%      Output vector (k=3):
-%
-%      [------|------|------------|------------------------]
-%	  |	  |	   |		    |
-%	  |	  |	   |		    `-> 1st scale coefficients 
-% 	  |	  |	   `-----------> 2nd scale coefficients
-%	  |	  `--------------------> 3rd scale coefficients
-%	  `----------------> Low pass residue  at 3rd scale 
-%
-%       
-%      If X is a matrix, the result will be another matrix with 
-%      the same number of rows, holding each one its respective 
-%      transformation.
-%
-%      WT (X,H,G,K,DEL1,DEL2) calculates the wavelet transform of 
-%      vector X, but also allows the users to change the alignment
-%      of the outputs with respect to the input signal. This effect
-%      is achieved by setting to DEL1 and DEL2 the delays of H and
-%      G respectively. The default values of DEL1 and DEL2 are 
-%      calculated using the function WTCENTER. 
-%
-%      See also:  IWT, WT2D, IWT2D, WTCENTER, ISPLIT.
-%
-% $Id: wt.m,v 1.2 2004/07/15 04:25:06 matthewbrett Exp $
+% $Id: wt.m,v 1.3 2004/07/15 05:19:00 matthewbrett Exp $
 
 if nargin < 3
   error('Need data to transform and two filters');
@@ -52,10 +24,14 @@ end
 
 sz = size(x);
 n_dims = length(sz);
-if n_dims == 2 & all(sz > 1)
-  p_dims = [0 1];
-else
-  p_dims = ones(1, n_dims);
+p_dims = zeros(n_dims);
+if sz(2) > 1
+  p_dims(2) = 1;
+else 
+  p_dims(1) = 1; 
 end
-
+  
+if n_dims > 2
+  warning('wt does a 1D transform; use wtnd for N-D transforms');
+end
 y = wtnd(x, h, g, scales, del1, del2, p_dims);
