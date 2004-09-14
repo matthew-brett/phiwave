1e06f4a746496e58124b6bef4a7c2c1c3dd9fe4a
Matthew Brett <matthew.brett@gmail.com>
2004-09-14 06:54:27 +0000

    Filled in phido comments and calls

--- @phido/phido.m
+++ @phido/phido.m
@@ -1,11 +1,14 @@
-function [o, others] = phido(params,passf)
+function [o, others] = phido(des, params, passf)
 % phido - class constructor for MarsBaR design object
 % inputs [defaults]
-% params  - structure, either:
-%             containing SPM design or mardo object or 
-%             containing fields for phido object, which should include
-%             'des_struct', containing design structure, or 'mardo'
-%             containing mardo design structure.
+% des     - SPM design, which can be a mardo object, or another SPM
+%           design format recognized by the mardo constructor - see help mardo for
+%           details.
+% params  - structure containing fields for phido object, which are
+%           - wavelet     - phiwave wavelet object to transform images
+%           - scales      - scales for wavelet transform
+%           - wtprefix    - prefix for wavelet transformed files
+%           - maskthresh  - threshold for mask image
 % passf   - if 1, or not passed, will try children objects to see if
 %           they would like to own this design
 %
@@ -40,4 +43,46 @@ function [o, others] = phido(params,passf)
 % constructor with passf set to 0 in order for the constructor merely to
 % make a phido object, without passing back to the other classes. 
 % 
-% $Id: phido.m,v 1.2 2004/09/14 05:31:23 matthewbrett Exp $
+% $Id: phido.m,v 1.3 2004/09/14 06:54:27 matthewbrett Exp $
+
+myclass = 'phido';
+defstruct = struct('vavelet',  phiw_lemarie(2), ...
+		   'scales',   4, ...
+		   'wtprefix', 'wv_', ...
+		   'maskthresh', 0.05);
+
+if nargin < 1
+  params = [];
+end
+if nargin < 2
+  passf = 1;
+end
+if isa(params, myclass)
+  o = params;
+  return
+end
+
+% send design to mardo
+mardo_o = mardo(des);
+if ~isa(mardo_o, 'mardo')
+  error('Could not create mardo design object with inputs');
+end
+
+% fill params with defaults, parse into fields for this object, children
+[pparams, others] = mars_struct('ffillsplit', defstruct, params);
+
+% add cvs tag
+pparams.cvs_version = mars_cvs_version(myclass);
+
+% set the phido object
+o  = class(pparams, myclass, mardo_o);
+
+% If requested, pass to child objects to request ownership
+if passf
+  o = phido_99(o, others);
+  if strcmp(class(o), myclass)
+    o = phido_2(o, others);
+  end
+end
+
+return
