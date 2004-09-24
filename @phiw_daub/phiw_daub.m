69eb132eebe943a679ca9b70d98aaa3037786697
Matthew Brett <matthew.brett@gmail.com>
2004-09-24 19:25:51 +0000

    Redesign of phiw_wavelet object, for more generic calling

--- @phiw_daub/phiw_daub.m
+++ @phiw_daub/phiw_daub.m
@@ -1,19 +1,65 @@
-function w = phiw_daub(params, inp)
+function [o, others] = phiw_daub(params, others)
 % phiw_daub - class constructor
+% FORMAT [o, others] = phiw_daub(params, others)
 % inherits from phiw_wavelet
-%
-% $Id: phiw_daub.m,v 1.1 2004/06/25 15:20:43 matthewbrett Exp $
+% 
+% Synopsis
+% --------
+% o = phiw_daub(4);
+% 
+% Inputs
+% params    - maybe a scalar, setting the number of coefficients for the
+%             Daubechies wavelet filter
+%             OR
+%             structure containing params field as above and values for
+%             any other object field for this object or parent (see
+%             phiw_wavelet for details)
+% others    - optional structure with any other fields for the object
+%             (see phiw_wavelet) 
+% 
+% $Id: phiw_daub.m,v 1.2 2004/09/24 19:25:51 matthewbrett Exp $
 
-myname = 'phiw_daub';  
+myclass = 'phiw_daub'; 
+cvs_v   = mars_cvs_version(myclass);
+
+% Default object structure; Daub filter with 4 coefficients
+defstruct = struct('params', 4);
+
 if nargin < 1
-  params = [];
-end
-if nargin < 2
-  inp = 1; % right is detail (UviWave)
-end
-if isa(params, myname)
-  w = params;
+  defstruct.cvs_version = cvs_v;
+  o = class(defstruct, myclass);
+  others = [];
   return
 end
-a.daub = '';
-w = class(a, myname, phiw_wavelet(params,inp));
+if nargin < 2
+  others = [];
+end
+
+if isa(params, myclass)
+  o = params;
+  % Check for simple form of call
+  if isempty(others), return, end
+
+  % Otherwise, we are being asked to set fields of object
+  % (There aren't any for now, so just sort out input args)
+  [p others] = mars_struct('split', others, defstruct);
+  return
+end
+
+% Check params input argument
+if ~isfield(params, 'params'), params = struct('params', params); end
+n_c = mars_struct('getifthere', params, 'params');
+if isempty(n_c)
+  error('Need params field in input struct, or scalar');
+end
+if ~isnumeric(others) | prod(size(others)) > 1
+  error('params argument should be a scalar');
+end
+
+% cvs version
+params.cvs_version = cvs_v;
+
+% set the phiw_wavelet object
+[phiw_w others] = phiw_wavelet(params, others);
+[o, others  = class(params, myclass);
+
--- @phiw_wavelet/phiw_wavelet.m
+++ @phiw_wavelet/phiw_wavelet.m
@@ -1,32 +1,85 @@
-function w = phiw_wavelet(params, flag)
+function [o, others] = phiw_wavelet(params, others)
 % phiw_wavelet - class constructor
-% inputs [defaults]
+% FORMAT [o, others] = phiw_wavelet(params, others)
+%
+% Synopsis
+% --------
+% o = phiw_wavelet;
+% 
+% % Imagine you have set variables H G RH RG to be analysis low, high pass
+% % synthesis low, high pass filters respectively
+% o = phiw_wavelet(struct('H', H, 'G', G, 'RH', RH, 'RG', RG));
+% 
+% Inputs [defaults]
 %  params  - maybe a filter structure containing fields
-%             H  - analysis low pass  [1]
-%             G  - analysis high pass [1]
-%             RH - synthesis low pass [1]
-%             RG - synthesis high pass[1]
+%              H  - analysis low pass   [1]
+%              G  - analysis high pass  [1]
+%              RH - synthesis low pass  [1]
+%              RG - synthesis high pass [1]
+%            OR
+%              structure containing params structure and any other object
+%              fields (being, at present, only detail_right (see below))
 %
-%  flag     - specifies detail coeffs to right (UviWave) [1]
+% others   - optional structure with any other fields for the object
+%            Fields can be 
+%             - detail_right - flag, if == 1 specifies detail coeffs to
+%                              right of vector (UviWave) [1]
+%            OR
+%            (if scalar) - interpreted as value for detail_right
 %
-% $Id: phiw_wavelet.m,v 1.2 2004/06/28 15:50:27 matthewbrett Exp $
-  
+% As usual, any unrecognized fields in input structures are passed out
+% for other (child) objects to parse if they like
+%
+% $Id: phiw_wavelet.m,v 1.3 2004/09/24 19:25:51 matthewbrett Exp $
+
+myclass = 'phiw_wavelet'; 
+cvs_v   = mars_cvs_version(myclass);
+
+% Default object structure
+defstruct = struct('params', struct('H',1,'G',1,'RH',1,'RG',1),...
+		   'detail_right', 1);
+
 if nargin < 1
-  params = [];
+  defstruct.cvs_version = cvs_v;
+  o = class(defstruct, myclass);
+  others = [];
+  return
 end
 if nargin < 2
-  % flag to specify detail coeffs go to right
-  % as for UviWave
-  flag = 1;  
+  others = [];
 end
-if isempty(params)
-  % inp1 can be wavelet filter structure
-  params = struct('H',1,'G',1,'RH',1,'RG',1);
+
+% If others is scalar, assume it is value for detail_right
+if isnumeric(others) & prod(size(others)) == 1
+  others = struct('detail_right', others);
 end
-if isa(params, 'phiw_wavelet')
-  w = params;
-else
-  s.rightf = flag;
-  s.params = params;
-  w = class(s, 'phiw_wavelet');
+if ~isempty(others) & ~isstruct(others)
+  error('"others" input should be scalar or struct');
 end
+
+if isa(params, myclass)
+  o = params;
+  % Check for simple form of call
+  if isempty(others), return, end
+
+  % Otherwise, we are being asked to set fields of object
+  [p others] = mars_struct('split', others, defstruct);
+  if isfield(p, 'detail_right'), o.detail_right = p.detail_right; end
+  return
+end
+
+% Check params input argument
+if ~isfield(params, 'params'), params = struct('params', params); end
+
+% fill with other params, defaults, parse into fields for this object,
+% children
+params = mars_struct('ffillmerge', params, others);
+[params, others] = mars_struct('ffillsplit', defstruct, params);
+
+% cvs version
+params.cvs_version = cvs_v;
+
+% set the phiw_wavelet object
+o  = class(params, myclass);
+
+return
