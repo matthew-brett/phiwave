b1158e405160d5a8bb536e840d8327837156eb32
Matthew Brett <matthew.brett@gmail.com>
2004-09-26 03:52:41 +0000

    New classdata method to fetch classdata via object

---
+++ @phiw_wavelet/classdata.m
@@ -0,0 +1,7 @@
+function cdata = classdata(obj, varargin)
+% classdata method - sets/gets class data
+% See help for private/pr_classdata for more details
+%
+% $Id: classdata.m,v 1.1 2004/09/26 03:52:41 matthewbrett Exp $
+
+cdata = pr_classdata(varargin{:});
