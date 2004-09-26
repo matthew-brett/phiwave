64c044c52b732fa99195addf35d3987693f0683c
Matthew Brett <matthew.brett@gmail.com>
2004-09-26 03:55:09 +0000

    New method (stolen from maroi method in marsbar) to get / set classdata

---
+++ @phiw_wavelet/private/pr_classdata.m
@@ -0,0 +1,52 @@
+function cdata = pr_classdata(fieldname, value)
+% pr_classdata method - sets/gets class data
+% phiw_wvavelet class data is implemented with a persistent variable
+% - CLASSDATA.  This is a structure containing fields
+%
+% wtcentermethod   - centre method for wavelet transform.  Integer in the
+%                    range 0:3; see help for center.m for details
+%
+% Field values can be returned with the call
+%    phiw_wavelet('classdata') - which returns the whole structure
+% or phiw_wavelet('classdata', fieldname) - which returns field data
+%
+% Field values can be set with the call
+% phiw_wavelet('classdata', fieldname, value) OR
+% phiw_wavelet('classdata', struct) where struct contains fields matching
+% those in CLASSDATA
+%
+% The same functionality results from 
+% classdata(obj, fieldname) etc.
+%
+% $Id: pr_classdata.m,v 1.1 2004/09/26 03:55:09 matthewbrett Exp $
+
+persistent CLASSDATA
+if isempty(CLASSDATA)
+  CLASSDATA = struct(...
+      'wtcentermethod', 0);
+end
+
+if nargin < 1 % simple classdata call
+  cdata = CLASSDATA;
+  return
+end
+if nargin < 2 & ~isstruct(fieldname) % fieldname get call
+  if isfield(CLASSDATA, fieldname) 
+    cdata = getfield(CLASSDATA,fieldname);
+  else 
+    cdata = []; 
+  end
+  return
+end
+
+% some sort of set call
+if ~isstruct(fieldname) 
+  fieldname = struct(struct(fieldname, value));
+end
+for field = fieldnames(fieldname)
+  if isfield(CLASSDATA, field{1})
+    CLASSDATA = setfield(CLASSDATA, field{1},...
+				    getfield(fieldname, field{1}));
+  end
+end
+cdata = CLASSDATA;
