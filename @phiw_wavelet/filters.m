8b135041e0b3cc7cbe1be8ecc8435e85ce2c59d0
Matthew Brett <matthew.brett@gmail.com>
2004-09-26 07:46:43 +0000

    Function moved to get_filters.m

--- @phiw_wavelet/filters.m
+++
@@ -1,7 +0,0 @@
-function [H,G,RH,RG] = filters(obj, varargin)
-% returns filters for template wavelet  
-%
-% $Id: filters.m,v 1.1 2004/06/25 15:20:43 matthewbrett Exp $
-
-w = obj.params;
-[H,G,RH,RG] = deal(w.H,w.G,w.RH,w.RG);
