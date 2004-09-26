9fa0b883652b98e1e78e4e02805705365f71d762
Matthew Brett <matthew.brett@gmail.com>
2004-09-26 07:54:40 +0000

    Actually sets num_coeffs field now, rather than just phiw_wavelet filters

--- @phiw_daub/num_coeffs.m
+++ @phiw_daub/num_coeffs.m
@@ -5,13 +5,14 @@ function res = num_coeffs(o, num)
 % get: n = num_coeffs(o);
 % set: o = num_coeffs(o, num)
 %
-% $Id: num_coeffs.m,v 1.1 2004/09/26 03:51:49 matthewbrett Exp $
+% $Id: num_coeffs.m,v 1.2 2004/09/26 07:54:40 matthewbrett Exp $
   
 if nargin < 2
   % get
   res = o.num_coeffs;
 else
   % set
+  o.num_coeffs = num;
   [H G RH RG] = daub(num);
   res = set_filters(o, H, G, RH, RG);
 end
