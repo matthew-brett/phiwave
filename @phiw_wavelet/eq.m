2c8810a666ec8853ef1508667843b0aef74fef6c
Matthew Brett <matthew.brett@gmail.com>
2004-11-18 19:00:14 +0000

    Returns 0 if attempt to compare objects of different class

--- @phiw_wavelet/eq.m
+++ @phiw_wavelet/eq.m
@@ -1,6 +1,9 @@
 function r = eq(obj1, obj2)
 % eq - == overloaded 
 %
-% $Id: eq.m,v 1.1 2004/06/25 15:20:43 matthewbrett Exp $
+% $Id: eq.m,v 1.2 2004/11/18 19:00:14 matthewbrett Exp $
 
+myclass = 'phiw_wavelet';
+
+if ~(isa(obj1, myclass) & isa(obj2, myclass)), r = 0; return, end
 r = strcmp(descrip(obj1), descrip(obj2));
