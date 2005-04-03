d4f0669ecc7cc85f5ecc306d1749aef19d4cf2b6
Matthew Brett <matthew.brett@gmail.com>
2005-04-03 07:24:32 +0000

    Always return object with img replaced by vol struct, even if just saved

--- @phiw_wvimg/thin.m
+++ @phiw_wvimg/thin.m
@@ -4,7 +4,7 @@ function thinobj = thin(obj, nosavef)
 % where nosavef prevents object image save even if there
 % have been changes to the object since creation
 %
-% $Id: thin.m,v 1.1 2004/06/25 15:20:43 matthewbrett Exp $
+% $Id: thin.m,v 1.2 2005/04/03 07:24:32 matthewbrett Exp $
   
 if nargin < 2
   nosavef = 0;
@@ -13,5 +13,5 @@ if obj.changef & ~nosavef
   thinobj = write_wtimg(obj);
 else
   thinobj = obj;
-  thinobj.img = thinobj.wvol;
 end
+thinobj.img = thinobj.wvol;
