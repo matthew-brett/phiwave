a9a0eedcf1889d5b7f83bbf0cbea1e4b8f1e2788
Matthew Brett <matthew.brett@gmail.com>
2005-04-06 22:35:10 +0000

    Added maskthresh field to passed parameters, renamed parameters to params from wtinfo

--- phiw_wvmask.m
+++ phiw_wvmask.m
@@ -1,38 +1,41 @@
 function wtobj = phiw_wvmask(voxmask,wtinfo,options)
 % phiw_wvmask - returns wavelet eqivalent mask for given voxel mask
 %
-% $Id: phiw_wvmask.m,v 1.2 2004/06/25 16:18:22 matthewbrett Exp $
+% $Id: phiw_wvmask.m,v 1.3 2005/04/06 22:35:10 matthewbrett Exp $
   
 if nargin < 1
   voxmask = spm_get(1,'img','Voxel mask');
 end
 if nargin < 2
-  wtinfo = [];
+  params = [];
 end
 if nargin < 3
   options = struct('datatype','uint8');
 end
 
-wtinfo = mars_struct('fillafromb', ...
-		     wtinfo, struct('scales',1,'wavelet',phiw_wavelet));
+params = mars_struct('fillafromb', ...
+		     params, ...
+		     struct('scales',1, ...
+			    'wavelet',phiw_wavelet, ...
+			    'maskthresh', 0.05));
 
 if ischar(voxmask),voxmask=spm_vol(voxmask);end
 
 % get unity wavelet
-wv1 = unitywavelet(wtinfo.wavelet);
+wv1 = unitywavelet(params.wavelet);
 
 % wt voxel mask
-wtobj = phiw_wvimg(voxmask,options,wv1,wtinfo.scales);
+wtobj = phiw_wvimg(voxmask,options,wv1,params.scales);
 
 % levels, quadrants
-[tmp qs nquads] = levels(wv1,size(wtobj.img),wtinfo.scales);
+[tmp qs nquads] = levels(wv1,size(wtobj.img),params.scales);
 
 % expansion required
-ex = width(wtinfo.wavelet)/2;
+ex = width(params.wavelet)/2;
 
 % cycle over blocks to do smoothing
-for l = 1:wtinfo.scales+1
-  if l > wtinfo.scales
+for l = 1:params.scales+1
+  if l > params.scales
     % at top level -> only one quadrant
     nquads = 2;
   end
@@ -44,4 +47,4 @@ for l = 1:wtinfo.scales+1
 end
 
 % rebinarize
-wtobj.img(wtobj.img>0) = 1;
+wtobj.img(wtobj.img>params.maskthresh) = 1;
