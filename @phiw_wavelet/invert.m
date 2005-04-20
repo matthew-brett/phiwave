4e8a70f68437bce22fc7d19fc355b8f8fd7ca7b2
Matthew Brett <matthew.brett@gmail.com>
2005-04-20 15:18:23 +0000

    Changed to use fast iwtnd function for inversion

--- @phiw_wavelet/invert.m
+++ @phiw_wavelet/invert.m
@@ -11,7 +11,7 @@ function data = invert(wimg, w, scales, oimgi)
 % Output
 % data    - wavelet inverted data, in resized data matrix if necessary
 %
-% $Id: invert.m,v 1.1 2004/06/25 15:20:43 matthewbrett Exp $
+% $Id: invert.m,v 1.2 2005/04/20 15:18:23 matthewbrett Exp $
  
 if nargin < 4
   oimgi = []; % do not reembed in original matrix
@@ -20,40 +20,14 @@ end
 wdims = size(wimg);
 numdims = ndims2(wdims);
 
-% inverting transform
+% interting transform
 levs = levels(w, wdims, scales);
 for d = 1:numdims
-  [t t2 fs(d).sH fs(d).sK] = filters(w,wdims(d));
+  [t1 t2 RH{d} RG{d}] = get_filters(w,wdims(d));
 end
 
 % in (up to) 3d
-switch(numdims)
- case 1
-  data = iwt(wimg,fs.sH,fs.sK,scales);
- case 2
-  data = iwt2d(wimg,fs.sH,fs.sK,scales);
-case 3
-  for d=scales:-1:1,
-    levinds = lims2subs(levs{d});
-    level = subsref(wimg, levinds);
-    [nx ny nz] = size(level);
-    % Inverting in the z axis
-    for x=1:nx, 
-      for y=1:ny,
-	level(x,y,:) = iwt(level(x,y,:),fs(3).sH,fs(3).sK,1);
-      end
-    end
-
-    % Inverting in the x-y plane
-    for z=1:nz, 
-      level(:,:,z) = iwt2d(level(:,:,z),fs(1).sH,fs(1).sK,1);
-    end
-    
-    wimg = subsasgn(wimg,levinds,level);
-  end
- otherwise
-  error('Haven''t done more than three dimensions yet')
-end
+wimg = iwtnd(wimg, RH, RG, scales);
 
 % restore to original dimensions as necessary
 if ~isempty(oimgi)
@@ -63,3 +37,6 @@ else
 end
 
 return
+
+
+
