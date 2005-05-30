cce6bcbea81ccd383c558ef158cab368e8b1f470
Matthew Brett <matthew.brett@gmail.com>
2005-05-30 16:52:44 +0000

    Default fill -> ffillsplit, pass options into estimation

--- @phido_99/estimate_wted.m
+++ @phido_99/estimate_wted.m
@@ -2,9 +2,13 @@ function [phiwD] = estimate_wted(phiwD, params)
 % do_estimate method - estimates phiwave GLM for SPM99 model
 %
 % phiwD           - phido_99 design object (containing SPM99 design)
-% params          - struct containing options (not used for now)
+% params          - struct containing fields, specifying options
+%                   Only option so far: 
+%                   'write_res' - if not 0, write residual images
 %
-% $Id: estimate_wted.m,v 1.2 2005/04/06 22:39:40 matthewbrett Exp $
+% $Id: estimate_wted.m,v 1.3 2005/05/30 16:52:44 matthewbrett Exp $
+
+def_params = struct('write_res', 1);
 
 if nargin < 2
   error('Need design and images');
@@ -13,11 +17,13 @@ if nargin < 3
   params = [];
 end
 
+params = mars_struct('ffillsplit', def_params, params);
+
 % get SPM design structure
 SPM = des_struct(phiwD);
   
 % do estimation
-SPM = pr_estimate(SPM);
+SPM = pr_estimate(SPM, [], params);
 
 % We must set SPMid to contain SPM99 string in order for the mardo_99 to
 % recognize this as an SPM99 design
