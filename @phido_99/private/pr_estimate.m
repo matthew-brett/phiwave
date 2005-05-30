7209dce787d36ae8e8978ab108384fd6749f8482
Matthew Brett <matthew.brett@gmail.com>
2005-05-30 16:53:19 +0000

    Write out residual images if flag is set

--- @phido_99/private/pr_estimate.m
+++ @phido_99/private/pr_estimate.m
@@ -1,7 +1,7 @@
-function SPM = pr_estimate(SPM, VY)
+function SPM = pr_estimate(SPM, VY, params)
 % Wavelet analysis enabled version of spm_spm.m
 % Estimation of the General Linear Model
-% FORMAT SPM = pr_estimate(SPM, VY)
+% FORMAT SPM = pr_estimate(SPM, VY, params)
 %
 % Based on:
 % @(#)spm_spm.m	2.37 Andrew Holmes, Jean-Baptiste Poline, Karl Friston 99/11/26
@@ -12,279 +12,42 @@ function SPM = pr_estimate(SPM, VY)
 % 3) Beta, ResMS image have wtprefix appended
 % 4) xCon is not saved separately, but in output file
 % 5) Design returned in SPM output structure
+% 6) Option to write out residual images during estimation
 %
 % VY    - nScan x nVar struct array of mapped image volumes
 %         Images must have the same orientation, voxel size and data type
 %       - Any scaling should have already been applied via the image handle
 %         scalefactors.
+%       - defaults to images stored in design
 %
-% SPM.xX    - Structure containing design matrix information
-%       - Required fields are:
-%         xX.X      - Design matrix (raw, not temporally smoothed)
-%         xX.Xnames - cellstr of parameter names corresponding to columns
-%                     of design matrix
-%       - Optional fields are:
-%         xX.K      - Sparse temporal smoothing matrix (see spm_sptop)
-% 		    - Or cell of session-specific structures (see spm_filter)
-%                   - Design & data are smoothed using K
-%                     (model is K*Y = K*X*beta + K*e)
-%                   - Note that K should not smooth across block boundaries
-%                   - defaults to speye(size(xX.X,1))
-%         xX.xVi    - structure describing intrinsic temporal auto-correlation
-%                   - required fields are
-%                     xX.xVi.Vi - intrinsic temporal auto-correlation matrix
-%                               - defaults to speye(size(xX.X,1))
+% params - structure containing options
 %
-% SPM.xM    - Structure containing masking information, or a simple column vector
-%         of thresholds corresponding to the images in VY.
-%       - If a structure, the required fields are:
-%         xM.TH - nVar x nScan matrix of analysis thresholds, one per image
-%         xM.I  - Implicit masking (0=>none, 1=>implicit zero/NaN mask)
-%         xM.VM - struct array of mapped explicit mask image volumes
-% 		- (empty if no explicit masks)
-%               - Explicit mask images are >0 for valid voxels to assess.
-%               - Mask images can have any orientation, voxel size or data
-%                 type. They are interpolated using nearest neighbour
-%                 interpolation to the voxel locations of the data Y.
-%       - Note that voxels with constant data (i.e. the same value across
-%         scans) are also automatically masked out.
+% For detailed help on the mathematics, structures etc, see spm_spm.m in
+% the SPM99 distribution - version string above.
 %
-% SPM.F_iX0 - Indicies of design matrix columns to form the reduced design
-%         matrix. The ensuing F-test (implemented through F-contrasts)
-%         is labelled "effects of interest", and is to filter the saved
-%         data pointlist (Y.mad).
-%       - If F_iX0 is a structure array
-%	  F_iX0.iX0  - indices as above
-%	  F_iX0.name - name of F-contrast
-%       - See Christensen for details of F-contrasts
-%
-%_______________________________________________________________________
-%
-% spm_spm is the heart of the SPM package. Given image files and a
-% General Linear Model, it estimates the model parameters, residual
-% variance, and smoothness of the standardised residual fields, writing
-% these out to disk in the current working directory for later
-% interrogation in the results section. (NB: Existing analyses in the
-% current working directory are overwritten.)
-%
-% The model is expressed via the design matrix (xX.X). The basic model
-% at each voxel is of the form is Y = X*beta * e, for data Y, design
-% matrix X, (unknown) parameters B, and residual errors e. The errors
-% are assummed to be independent and identically Normally distributed
-% with zero mean and unspecified variance. The parameters are estimated
-% by ordinary least squares, using matrix methods, and the residual
-% variance estimated.
-%
-% Note that only a single variance component is considered. This is
-% *always* a fixed effects model. Random effects models can be effected
-% using summary statistics, summarising the data such that the fixed
-% effects in the model on the summary data are those effects of
-% interest (i.e. the population effects). In this case the residual
-% variance for the model on the summary data contains the appropriate
-% variance components in the appropriate ratio. See spm_RandFX.man for
-% further details.
-%
-% Under the additional assumption that the standardised residual images
-% are strictly stationary standard Gaussian random fields, results from
-% Random field theory can be applied to estimate the significance
-% statistic images (SPM's) accounting for the multiplicity of testing
-% at all voxels in the volume of interest simultaneously. The required
-% parameters are the volume, and Lambda, the variance covariance matrix
-% of partial derivatives of the standardised error fields. The
-% algorithm estimates the variances of the partial derivatives in the
-% axis directions (the diagonal of Lambda). The covariances (off
-% diagonal elements of Lambda) are assumed zero.
-% 
-%                           ----------------
-%
-% For temporally correlated (fMRI) data, the algorithm implements the
-% technique of Worsley & Friston (1995). In this approach the model is
-% assummed to fit such that the residuals have (slight) short-term
-% (intrinsic) autocorrelation given by Vi. (I.e. Residuals e =
-% sqrt(Vi)*e', where e' is an (unobserved) white noise time series).
-% The data and model are then temporally filtered prior to model
-% fitting by a linear filter given by matrix K, giving model K*Y =
-% K*X*beta + K*e. K=inv(sqrt(Vi)) corresponds to pre-whitening (leaving
-% residuals e'), K=I to no temporal smoothing (appropriate for
-% independent data when Vi=I), and K=S (a temporal filter matrix - see
-% spm_make_filter) for band-pass filtering.
-% 
-% The autocorrelation in the filtered time series is then K*Vi*K'
-% Standard results for serially correlated regression can then be used
-% to produce variance estimates and effective degrees of freedom
-% corrected for the temporal auto-correlation. Note that these
-% corrections give non-integer degrees of freedom.
-% 
-% Vi can be passed as a parameter. If Vi is not specified then it is
-% taken as I. Often Vi is unknown (or spatially variant, or just
-% difficult to estimate). Provided the intrinsic autocorrelation Vi is
-% small relative to that imposed by temporal smoothing (i.e. the
-% temporal smoothing swamps the intrinsic autocorrelation), the
-% variance-covariance structure of the residuals is approximately that
-% imposed by the smoothing, i.e K*Ve*K' ~ K*Vi*K' (Ve = estimated Vi).
-% if xX.xVi.row{n} is specified Vi is estimated using an AR(1) model
-% for each session (n). 
-% 
-% The volume analsed is the intersection of the threshold masks,
-% explicit masks and implicit masks. See spm_spm_ui for further details
-% on masking options.
-%
-%                           ----------------
-%
-% MV:
-% If the response variable is multivariate (i.e. size(VY,2) > 1) then
-% spm_spm proceeds with a voxel by voxel ManCova to produce a SPM{F}
-% based on Wilks Lambda for all effects of interest (specified by F_iX0).
-% The ensuing parameter estimates, data (Y.mad) and residual sum of squares
-% pertain to the first canonical variate. This is the linear combination
-% of response variables that maximizes the sum of squares explained
-% by the effects of interest relative to error.  Because there is only
-% one contrast (F_iX0) the SPM{F} is created at this point and details
-% are saved in xCon.
-% 
-%-----------------------------------------------------------------------
-%
-% The output of spm_spm takes the form of an SPM.mat file of analysis
-% parameters, and 'float' Analyze images of the parameter and variance
-% estimates. An 8bit zero-one mask image indicating the volume assessed
-% is also written out, with zero indicating voxels outside tha analysed
-% volume.
-%
-%                           ----------------
-%
-% SPM.mat                                         - analysis parameters
-% The SPM.mat file contains the following variables:
-%     SPMid     - string identifying SPM & program version
-%     VY        - as input
-%
-%     xX        - design matrix structure
-%               - all the fields of the input xX are retained
-%               - the following additional fields are added:
-%     xX.V      - V matrix (xX.K*xX.xVi.Vi*xX.K')
-%     xX.xKXs   - space structure for K*X (xX.K*xX.X), the temporally smoothed
-%                 design matrix
-%               - given as spm_sp('Set',xX.K*xX.X) - see spm_sp for details
-%                 (so xX.xKXs.X is K*X)
-%     xX.pKX    - pseudoinverse of K*X (xX.K*xX.X), computed by spm_sp
-%                 (this was called PH in spm_AnCova)
-%     xX.pKXV   - pinv(K*X)*V  (this was called PHV in spm_AnCova)
-%     xX.Bcov   - pinv(K*X)*V*pinv(K*X)' - variance-covariance matrix
-%                 of parameter estimates (when multiplied by ResMS)
-%                 (NB: BCOV in SPM96 was xX.Bcov/xX.trRV, so that BCOV    )
-%                 (multiplied by ResSS was the variance-covariance matrix )
-%                 (of the parameter estimates. (ResSS/xX.trRV = ResMS)    )
-%     xX.trRV   - trace of R*V, computed efficiently by spm_SpUtil
-%     xX.trRVRV - trace of RVRV (this was called trRV2 in spm_AnCova)
-%     xX.erdf   - effective residual degrees of freedom (trRV^2/trRVRV)
-%     xX.nKX    - design matrix (xX.xKXs.X) scaled for display
-%                 (see spm_DesMtx('sca',... for details)
-% 
-%     xM        - as input (but always in the structure format)
-%
-%     M         - 4x4 voxel->mm transformation matrix
-%     DIM       - image dimensions {voxels} - column vector
-%     Vbeta     - cellstr of beta image filenames (relative)
-%     VResMS    - relative filename of ResMS image
-%     XYZ       - 3xS (S below) vector of in-mask voxel coordinates
-%     F_iX0     - structure array of default F-contrasts
-%     F_iX0.iX0 - as input
-%     F_iX0.name- as input
-%     S         - Lebesgue measure or volume (in voxels)
-%
-% ...plus any additional arguments passed to spm_spm for saving in the
-%    SPM.mat file
-%
-%                           ----------------
-%
-% xCon.mat                                        - contrast definitions
-% The xCon.mat file contains a single structure defining the default F-contrast
-% (The results section can be used to define additional contrasts.)
-%     xCon       - Contrast structure (created by spm_FcUtil.m)
-%     xCon.name  - Name of contrast
-%     xCon.STAT  - 'F', 'T' or 'X' - for F/T-contrast ('X' for multivariate)
-%     xCon.c     - (F) Contrast weights
-%     xCon.X0    - Reduced design matrix (spans design space under Ho)
-%                  It is in the form of a matrix (spm99b) or the
-%                  coordinates of this matrix in the orthogonal basis
-%                  of xX.X defined in spm_sp. 
-%     xCon.iX0   - Indicates how contrast was specified:
-%                  If by columns for reduced design matrix then iX0 contains the
-%                  column indices. Otherwise, it's a string containing the
-%                  spm_FcUtil 'Set' action: Usuall one of {'c','c+','X0'}
-%                  (Usually this is the input argument F_iX0.)
-%     xCon.X1o   - Remaining design space (orthogonal to X0).
-%                  It is in the form of a matrix (spm99b) or the
-%                  coordinates of this matrix in the orthogonal basis
-%                  of xX.X defined in spm_sp.
-%     xCon.eidf  - Effective interest degrees of freedom (numerator df)
-%     xCon.Vcon  - ...for handle of contrast/ESS image (empty at this stage)
-%     xCon.Vspm  - ...for handle of SPM image (empty at this stage)
-%
-%                           ----------------
-%
-% mask.{img,hdr}                                   - analysis mask image
-% 8-bit (uint8) image of zero-s & one's indicating which voxels were
-% included in the analysis. This mask image is the intersection of the
-% explicit, implicit and threshold masks specified in the xM argument.
-% The XYZ matrix contains the voxel coordinates of all voxels in the
-% analysis mask. The mask image is included for reference, but is not
-% explicitly used by the results section.
-%
-%                           ----------------
-%
-% beta_????.{img,hdr}                                 - parameter images
-% These are 16-bit (float) images of the parameter estimates. The image
-% files are numbered according to the corresponding column of the
-% design matrix. Voxels outside the analysis mask (mask.img) are given
-% value NaN.
-%
-%                           ----------------
-%
-% ResMS.{img,hdr}                    - estimated residual variance image
-% This is a 32-bit (double) image of the residual variance estimate.
-% Voxels outside the analysis mask are given value NaN.
-%
-%-----------------------------------------------------------------------
-%
-% References:
-%
-% Christensen R (1996) Plane Answers to Complex Questions
-%       Springer Verlag
-%
-% Friston KJ, Holmes AP, Worsley KJ, Poline JP, Frith CD, Frackowiak RSJ (1995)
-% ``Statistical Parametric Maps in Functional Imaging:
-%   A General Linear Approach''
-%       Human Brain Mapping 2:189-210
-%
-% Worsley KJ, Friston KJ (1995)
-% ``Analysis of fMRI Time-Series Revisited --- Again''
-%       Neuroimage 2:173-181
-%
-%-----------------------------------------------------------------------
-%
-% For those interested, the analysis proceeds a "plank" at a time,
-% where a "plank" is a bunch of lines (parallel to the x-axis) within a
-% plane. The plank width is determined at run-time as the maximum
-% possible (at most the plane width) plank width satisfying the memory
-% constraint parameterised in the code in variable maxMem. maxMem can
-% be tweaked for your system by editing the code.
-%
-%_______________________________________________________________________
-% @(#)spm_spm.m	2.37 Andrew Holmes, Jean-Baptiste Poline, Karl Friston 99/11/26
-%
-% $Id: pr_estimate.m,v 1.2 2005/04/20 15:16:40 matthewbrett Exp $
+% $Id: pr_estimate.m,v 1.3 2005/05/30 16:53:19 matthewbrett Exp $
 
 %-Condition arguments
 %-----------------------------------------------------------------------
+def_params = struct('write_res', 1);
+
 if nargin < 1
   error('Need SPM design structure');
 end
 if nargin < 2
+  VY = [];
+end
+if isempty(VY)
   if ~isfield(SPM, 'VY')
     error('Need data to estimate for');
   end
   VY = SPM.VY;
 end
+if nargin < 3
+  params = [];
+end
+params = mars_struct('ffillsplit', def_params, params);
+
 if ~isfield(SPM, 'xX'), error('Need design in SPM structure'); end
 xX = SPM.xX;
 if ~isfield(SPM, 'xM')
@@ -352,7 +115,7 @@ if exist(fullfile('.',phiw_mat_name),'file')==2
 	drawnow
 end
 
-files = { 'mask.???','ResMS.???',...
+files = { 'mask.???','ResMS.???','ResI_?????.???'...
 	  'beta_????.???','con_????.???',...
 	  'ess_????.???', 'spm?_????.???'};
 for i=1:length(files), files{i} = [wtp files{i}]; end
@@ -532,6 +295,26 @@ VResMS = struct(	'fname',	[wtp 'ResMS.img'],...
 			'descrip',	'phiwave:Residual sum-of-squares');
 VResMS = spm_create_image(VResMS);
 
+%-Intialise residual images
+%-----------------------------------------------------------------------
+if params.write_res
+  VResI(1:nScan) = deal(struct(...
+      'fname',  [],...
+      'dim',    [DIM',spm_type('double')],...
+      'mat',    M,...
+      'pinfo',  [1 0 0]',...
+      'descrip','spm_spm:Residual image'));
+  
+  for i = 1:nScan
+    VResI(i).fname   = sprintf('%sResI_%05d.img', wtp, i);
+    VResI(i).descrip = sprintf('spm_spm:ResI (%04d)', i);
+    spm_unlink(VResI(i).fname);
+    VResI(i) = spm_create_image(VResI(i));
+  end
+else
+  VResI = [];
+end
+
 %-Intialise multivariate SPMF file
 %-----------------------------------------------------------------------
 if nVar > 1
@@ -593,6 +376,7 @@ for z = 1:zdim				%-loop over planes (2D or 3D data)
 
     zords   = z*ones(xdim*ydim,1)';	%-plane Z coordinates
     CrBl    = [];			%-current plane betas
+    CrResI  = [];			%-normalized residuals	
     CrmvF   = [];			%-current plane mvF-squared
     CrResSS = [];			%-current plane ResSS
 
@@ -761,8 +545,9 @@ for z = 1:zdim				%-loop over planes (2D or 3D data)
 	% (analyze images, or *.mad and retreive when plane complete )
 	%---------------------------------------------------------------
 	CrBl 	= [CrBl,beta];
+	if params.write_res, CrResI  = [CrResI, res]; end
 	CrResSS = [CrResSS,ResSS];
-	
+
 	end % (nVox)
 
 	%-Append new inmask voxel locations and volumes
@@ -799,6 +584,15 @@ for z = 1:zdim				%-loop over planes (2D or 3D data)
 	Vbeta(i) = spm_write_plane(Vbeta(i),tmp,z);
     end
 
+    %-Write variance images
+    %-------------------------------------------------------------------
+    if params.write_res
+      for i = 1:nScan
+        if length(Q), tmp(Q) = CrResI(i,:); end
+	VResI(i) = spm_write_plane(VResI(i), tmp, z);
+      end
+    end
+    
     %-Write ResSS into ResMS (variance) image
     % (Scaling of ResSS to ResMS by trRV is accomplished by adjusting the
     % (scalefactor at the end, once the intrinsic temporal autocorrelation
@@ -889,6 +683,7 @@ VResMS                  = spm_create_image(VResMS);
 fprintf('%s%30s',sprintf('\b')*ones(1,30),'...tidying file handles') %-#
 VM     = VM.fname;
 Vbeta  = {Vbeta.fname}';
+if params.write_res, VResI = {{VResI.fname}'}; end
 VResMS = VResMS.fname;
 if nVar > 1
 	xCon.Vspm = Vspm.fname;
@@ -915,6 +710,7 @@ SPM = mars_struct('ffillmerge', SPM, ...
 		      'DIM',   DIM, ...
 		      'VM',    VM, ...
 		      'Vbeta', {Vbeta}, ...
+		      'VResI', VResI, ...
 		      'VResMS',VResMS, ...
 		      'XYZ',   XYZ, ...
 		      'F_iX0', F_iX0, ...
